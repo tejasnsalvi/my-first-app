2f57efcface7f528cd324abb67db8b5b560f728b
“Tejas <tejasnsalvi@gmail.com>
2024-02-28 00:30:52 +0530

    redux persist

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import { createStore } from 'redux'
 import allReducers from './reducers'
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- my-first-app/src/reducers/index.js
+++ my-first-app/src/reducers/index.js
@@ -1,10 +1,17 @@
 import {combineReducers} from 'redux';
 import counterReducer from './counter';
 import logReducer from './isLogged';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Defaults to localStorage for web and AsyncStorage for React Native
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
 
 const allReducers = combineReducers({
     counter : counterReducer,
     isLogged : logReducer
 })
 
-export default allReducers
+export default persistReducer(persistConfig,allReducers)
